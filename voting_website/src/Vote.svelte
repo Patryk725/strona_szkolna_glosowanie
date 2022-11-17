4b71e72489e2032dd3fec86cc14a829c04e9ea5f
beProsto <lavionperavion@gmail.com>
2022-11-18 00:25:36 +0100

    patryk pomocy z tym cssem błagam ;w;

--- voting_website/src/Vote.svelte
+++ voting_website/src/Vote.svelte
@@ -1,23 +1,33 @@
+<script>
+    import Card from "./Card.svelte";
+
+    function handleVoting() {
+    	alert('musisz być zalogowany, aby oddać głos');
+  	}
+
+	let teams = [];
+
+	fetch("src/assets/teams.json")
+	.then(res => {
+		res.json()
+		.then(json => {
+			teams = json;
+			console.log(teams);
+		});
+	});
+</script>
+
 <div class="voting">
     <div class="voting__header">
     <h2>Kto wygra mecz?</h2>
     </div>
-    <div class="voting__card">
-    <div class="card__img">
-        <img src="./src/assets/poland.png" alt="Poland">
-    </div>
-    <div class="card__desc">
-        <p>Polska</p>
-    </div>
-    </div>
-    <div class="voting__card">
-    <div class="card__img">
-        <img src="./src/assets/mexico.png" alt="Mexico">
-    </div>
-    <div class="card__desc">
-        <p>Meksyk</p>
-    </div>
-    </div>
+
+	<div id="team_view">
+		{#each teams as team}
+			<Card country_name="{team.name}" flag_image="{team.img}"/>
+		{/each}
+	</div>
+
     <div class="voting__submit">
         <button on:click={handleVoting}>
             Oddaj głos
@@ -25,8 +35,14 @@
     </div>
 </div>
 
-<script>
-    function handleVoting() {
-    alert('musisz być zalogowany, aby oddać głos');
-  }
-</script>
+<style>
+	#team_view {
+		overflow-y: scroll;
+		height: 70vh;
+		display: grid;
+		flex-direction: column;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 2rem;
+		width: 100%;
+	}
+</style>
