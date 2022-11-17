cb01bed03fa96955dfc557989e2094b6000b0cd9
Patryk725 <ziomski725@gmail.com>
2022-11-17 18:38:16 +0100

    polishing frontend

--- voting_website/src/App.svelte
+++ voting_website/src/App.svelte
@@ -13,6 +13,27 @@
     box-shadow: 0 0 2rem 1rem #00000050;
     margin: 0;
   }
+
+  .nav--login {
+    position: fixed;
+    top: 0;
+    right: 20px;
+    gap: .2rem;
+  }
+
+  .nav--login_list {
+    display: flex;
+    gap: 1.5rem;
+    list-style-type: none;
+    font-size: 1.1rem;
+    cursor: pointer;
+    padding: 1rem;
+  }
+
+  .option--login_option:hover {
+    text-decoration: underline;
+  }
+
   .section--right {
     flex: 3;
     display: grid;
@@ -39,6 +60,12 @@
     border-radius: .5rem;
     padding: 1rem;
     box-shadow: 0 0 .5rem .1rem #00000050;
+    cursor: pointer;
+    transition: .1s ease;
+  }
+  
+  .voting__card:hover {
+    transform: translateY(-2%);
   }
 
   .voting__card img {
@@ -48,14 +75,32 @@
     user-select: none;
   }
 
+
   .card__desc {
     text-align: center;
     font-weight: 500;
   }
 
+  .voting__submit {
+    grid-column-start: span 2;
+  }
+
   .voting__submit button {
-    padding: 1rem  10rem;
-    background-color: green;
+    width: 100%;
+    padding: 1.2rem 0;
+    border: none;
+    font-size: 1.2rem;
+    background-color: #349EDA;
+    cursor: pointer;
+    text-shadow: 0 0 .2rem #000000;
+    color: #ffffff;
+    border-radius: .5rem;
+    transition: .1s ease;
+    user-select: none;
+  }
+
+  .voting__submit button:hover {
+    background-color: #35B6FF;
   }
 </style>
 
@@ -64,6 +109,16 @@
     <div class="section--left">
     </div>
     <div class="section--right">
+      <nav class="nav--login">
+        <ul class="nav--login_list">
+          <li class="option--login_option">
+            zaloguj się
+          </li>
+          <li class="option--login_option">
+            profil
+          </li>
+        </ul>
+      </nav>
       <div class="voting">
         <div class="voting__header">
           <h2>Kto wygra mecz?</h2>
@@ -85,7 +140,7 @@
           </div>
         </div>
         <div class="voting__submit">
-          <button>
+          <button on:click={handleVoting}>
             Oddaj głos
           </button>
         </div>
@@ -95,5 +150,10 @@
 </main>
 
 <script>
-  
+  function handleVoting() {
+    alert('musisz być zalogowany, aby oddać głos');
+  }
+  function handleModal() {
+    this.showModal();
+  }
 </script>
