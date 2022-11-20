590be3f5a6d4bdff257c6f43b31b1a3bf19e48b7
Patryk725 <ziomski725@gmail.com>
2022-11-20 16:47:24 +0100

    added voting system

--- www/src/Card.svelte
+++ www/src/Card.svelte
@@ -10,4 +10,39 @@
 	<div class="card__desc">
 		<p>{country_name}</p>
 	</div>
+	<div class="trophies">
+		<div class="trophy">
+			<img src="src/assets/b_trophy.png" class="trophy__img" alt="trophy">
+			<img src="src/assets/s_trophy.png" class="trophy__img" alt="trophy">
+			<img src="src/assets/g_trophy.png" class="trophy__img" alt="trophy">
+		</div>
+	</div>
 </div>
+
+<style>
+
+	.voting__card {
+		position: relative;
+	}
+
+	.trophy__img {
+		position: absolute;
+		width: 48px;
+		height: 48px;
+		filter: drop-shadow(0 0 1px #00000090);
+		box-shadow: none;
+	}
+
+	.trophy__img:first-child {
+		top: -10px;
+		right: -25px;
+	}
+	.trophy__img:nth-child(2) {
+		top: -10px;
+		right: 0px;
+	}
+	.trophy__img:nth-child(3) {
+		top: -10px;
+		right: 25px;
+	}
+</style>
